cc8dcb2f75b82c9b09829061dcbb63d91d61900c
pjhpkm0727 <061116pjh@naver.com>
2023-06-21 14:02:09 +0900

    FEAT: Link home

--- drug_project/src/pages/main/index.jsx
+++ drug_project/src/pages/main/index.jsx
@@ -1,8 +1,15 @@
 import * as S from './styled';
+import Input from '../../components/input';
+import {Link} from 'react-router-dom';
 
 function Main(){
   return(
-    <S.Title>Root-page입니다.</S.Title>
+    <S.Container>
+      <S.LogoImage />
+      <Input type='text' placeholder='아이디를 입력하세요.' />
+      <Input type='password' placeholder='비밀번호를 입력하세요.' />
+      <Link to='/home'><Input type='submit' value='로그인' /></Link>
+    </S.Container>
   );
 }
 
