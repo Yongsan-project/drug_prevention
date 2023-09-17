2a8d2a79d21bd7676095c169e6c7edb49cf11e85
pjhpkm0727 <061116pjh@naver.com>
2023-09-17 13:44:06 +0900

    FEAT: create warning page

---
+++ drug_project/src/components/warning/index.jsx
@@ -0,0 +1,12 @@
+import * as S from './styled';
+import WarningImage from '../../image/icons8-warning-96.png';
+
+const Warning = () => {
+  return(
+    <S.Background>
+      <S.WarningImage src={WarningImage} alt='Warning!!!' />
+    </S.Background>
+  );
+}
+
+export default Warning;
